4831cb7e063b220183057757f45e77f2c2d152d6
Dani G. Mazzarolo <145726469+DaniMazza@users.noreply.github.com>
2024-03-06 22:11:14 -0300

    Add files via upload

---
+++ Fornecedor.java
@@ -0,0 +1,36 @@
+public class Fornecedor extends Pessoa{
+    private double credValue;
+    private double debtValue;
+
+    public Fornecedor(String name, String address, String phoneNumber, double credValue, double debtValue){
+        super(name, address, phoneNumber);
+        this.credValue = credValue;
+        this.debtValue = debtValue;
+    }
+    public Fornecedor(String name, double credValue, double debtValue){
+        super(name);
+        this.credValue = credValue;
+        this.debtValue = debtValue;
+    }
+
+
+    public double obterSaldo(double credValue, double debtValue){
+        return credValue - debtValue;
+    }
+
+    public double getCredValue() {
+        return credValue;
+    }
+
+    public double getDebtValue() {
+        return debtValue;
+    }
+
+    public void setDebtValue(double debtValue) {
+        this.debtValue = debtValue;
+    }
+
+    public void setCredValue(double credValue) {
+        this.credValue = credValue;
+    }
+}
