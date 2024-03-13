2e6c57f7295ec05e38653dc99a0decb22c06a716
Dani G. Mazzarolo <145726469+DaniMazza@users.noreply.github.com>
2024-03-13 19:03:00 -0300

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,72 +1,51 @@
-import java.util.Scanner;
-
 public class Tester {
-	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("What your first name? ");
-		String name = teclado.next();
-		System.out.println("How much credit do you have??");
-		double credValue = teclado.nextDouble();
-		System.out.println("How much debt do you have??");
-		double debtValue = teclado.nextDouble();
-
-
-		Fornecedor firstPeople = new Fornecedor(name, credValue, debtValue);
-		double saldo = firstPeople.obterSaldo(credValue, debtValue);
-		System.out.println("O fornecedor " + firstPeople.getName() + " tem um saldo de: " + saldo);
-
-
-		System.out.println("What your first name? ");
-		name = teclado.next();
-		System.out.println("what is your sector code?");
-		int codigoSetor = teclado.nextInt();
-
-		Empregado secondPeople = new Empregado(name, codigoSetor);
-		System.out.println("Empregado(a): " + name + "\n" +
-				"recebe um salario líquido de: " + secondPeople.calcularSalario(1000, 5)); 
-		teclado.close();
-		public class TestePessoas {
-
-    public static void main(String[] args) {
-
-        Fornecedor fornecedor1 = new Fornecedor("Gabriel", null, null, 300, 1000);
-        System.out.println("Fornecedor >>> "+ fornecedor1.getNome()+" - Saldo "+fornecedor1.obterSaldo());
-
-
-        Empregado empregado1 =
-                new Empregado("Daniel", null, null, 171, 10000, 27.5);
-        Empregado empregado2 =
-                new Administrador("Antonio", null, null, 171, 10000, 27.5, 500);
-
-        Empregado empregado3 =
-                new Operario("Davi", null, null, 171, 10000, 27.5,2000, 10);
-
-        Empregado empregado4 =
-                new Vendedor("Julia", null, null, 171, 10000, 27.5,10000, 20);
-
-
-
-        System.out.println("Empregado >>> "+ empregado1.getNome()+" - Salario "+empregado1.calcularSalario());
-        System.out.println("Empregado >>> "+ empregado2.getNome()+" - Salario "+empregado2.calcularSalario());
-        System.out.println("Empregado >>> "+ empregado3.getNome()+" - Salario "+empregado3.calcularSalario());
-        System.out.println("Empregado >>> "+ empregado4.getNome()+" - Salario "+empregado4.calcularSalario());
-
-
-        Pessoa p1 = fornecedor1;
-        Pessoa p2 = empregado1;
-
-        if(p1 instanceof Fornecedor) {
-            System.out.println("p1 instanceof Fornecedor >>>"+ (p1 instanceof Fornecedor));
-            /*
-             * cast de tipo, forçando a conversão  e fazendo com que o compilador ignore o erro de conversão
-             */
-            fornecedor1 = (Fornecedor) p1;
-            System.out.println("Fornecedor >>> "+ fornecedor1.getNome()+" - Saldo "+fornecedor1.obterSaldo());
+        private static void templateEmployee(Empregado empregado) {
+            String cargo = "Empregado(a)";
+            if (empregado instanceof Vendedor) {
+                cargo = "Vendedor(a)";
+            } else if (empregado instanceof Operario) {
+                cargo = "Operario(a)";
+            } else if (empregado instanceof Administrador) {
+                cargo = "Administrador(a)";
+            }
+            System.out.printf("|%-17s | %-13s | R$ %-6.0f |%n", cargo, empregado.getName(), empregado.getCalcularSalario());
         }
 
+        private static void titleEmployee() {
+            System.out.println("_________________");
+            System.out.printf("|%-17s | %-13s | %-9s |%n", "Cargo", "Name", "Salario");
+        }
 
+        private static void titleSupplier() {
+            System.out.println("_________________");
+            System.out.printf("|%-17s | %-13s | %-9s |%n", "Cargo", "Name", "Saldo");
+        }
 
-    }
+        private static void templateSupplier(Fornecedor fornecedor) {
+            System.out.printf("|%-17s | %-13s | R$ %-6.0f |%n", "Fornecedor(a)", fornecedor.getName(), fornecedor.obterSaldo());
+        }
+        private  static void finalLine() {
+            System.out.println("|________________|");
+
+        }
+
+        public static void main(String[] args) {
+            Fornecedor fornecedor1 = new Fornecedor("Gabriel", 300, 1000);
+            titleSupplier();
+            templateSupplier(fornecedor1.getName(), fornecedor1.obterSaldo());
+            finalLine();
+            Empregado empregado1 = new Empregado("Ziru", 171, 10000, 27.5);
+            Empregado empregado2 = new Administrador("Antonio", 171, 10000, 27.5, 500);
+            Empregado empregado3 = new Operario("Davi", 171, 10000, 27.5, 2000, 10);
+            Empregado empregado4 = new Vendedor("Julia", 171, 10000, 27.5, 10000, 20);
+            titleEmployee();
+            templateEmployee(empregado1, empregado1.getName(), empregado1.calcularSalario());
+            templateEmployee(empregado2, empregado2.getName(), empregado2.calcularSalario());
+            templateEmployee(empregado3, empregado3.getName(), empregado3.calcularSalario());
+            templateEmployee(empregado4, empregado4.getName(), empregado4.calcularSalario());
+            finalLine();
+        }
+}
 }
 
 	}
