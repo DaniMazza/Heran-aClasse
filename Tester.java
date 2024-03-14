563a8293cb620dbe984bce42cef7da3ddf7ef873
Dani G. Mazzarolo <145726469+DaniMazza@users.noreply.github.com>
2024-03-14 09:22:32 -0300

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -8,16 +8,16 @@ public class Tester {
             } else if (empregado instanceof Administrador) {
                 cargo = "Administrador(a)";
             }
-            System.out.printf("|%-17s | %-13s | R$ %-6.0f |%n", cargo, empregado.getName(), empregado.getCalcularSalario());
+            System.out.printf("|%-17s | %-13s | R$ %-6.0f |%n", cargo, empregado.getName(), empregado.calcularSalario());
         }
 
         private static void titleEmployee() {
-            System.out.println("_________________");
+            System.out.println("_______________________________________________");
             System.out.printf("|%-17s | %-13s | %-9s |%n", "Cargo", "Name", "Salario");
         }
 
         private static void titleSupplier() {
-            System.out.println("_________________");
+            System.out.println("_______________________________________________");
             System.out.printf("|%-17s | %-13s | %-9s |%n", "Cargo", "Name", "Saldo");
         }
 
@@ -25,29 +25,24 @@ public class Tester {
             System.out.printf("|%-17s | %-13s | R$ %-6.0f |%n", "Fornecedor(a)", fornecedor.getName(), fornecedor.obterSaldo());
         }
         private  static void finalLine() {
-            System.out.println("|________________|");
+            System.out.println("|______________________________________________|");
 
         }
 
         public static void main(String[] args) {
             Fornecedor fornecedor1 = new Fornecedor("Gabriel", 300, 1000);
             titleSupplier();
-            templateSupplier(fornecedor1.getName(), fornecedor1.obterSaldo());
+            templateSupplier(fornecedor1);
             finalLine();
             Empregado empregado1 = new Empregado("Ziru", 171, 10000, 27.5);
             Empregado empregado2 = new Administrador("Antonio", 171, 10000, 27.5, 500);
             Empregado empregado3 = new Operario("Davi", 171, 10000, 27.5, 2000, 10);
             Empregado empregado4 = new Vendedor("Julia", 171, 10000, 27.5, 10000, 20);
             titleEmployee();
-            templateEmployee(empregado1, empregado1.getName(), empregado1.calcularSalario());
-            templateEmployee(empregado2, empregado2.getName(), empregado2.calcularSalario());
-            templateEmployee(empregado3, empregado3.getName(), empregado3.calcularSalario());
-            templateEmployee(empregado4, empregado4.getName(), empregado4.calcularSalario());
+            templateEmployee(empregado1);
+            templateEmployee(empregado2);
+            templateEmployee(empregado3);
+            templateEmployee(empregado4);
             finalLine();
         }
 }
-}
-
-	}
-}
-
