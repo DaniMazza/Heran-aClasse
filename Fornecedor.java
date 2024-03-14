462ebdfba076765ccc39630d2062ad5d8e1f13b5
Dani G. Mazzarolo <145726469+DaniMazza@users.noreply.github.com>
2024-03-14 09:31:31 -0300

    Update Fornecedor.java

--- Fornecedor.java
+++ Fornecedor.java
@@ -1,36 +1,38 @@
 public class Fornecedor extends Pessoa{
-    private double credValue;
-    private double debtValue;
-
-    public Fornecedor(String name, String address, String phoneNumber, double credValue, double debtValue){
-        super(name, address, phoneNumber);
-        this.credValue = credValue;
-        this.debtValue = debtValue;
-    }
-    public Fornecedor(String name, double credValue, double debtValue){
-        super(name);
-        this.credValue = credValue;
-        this.debtValue = debtValue;
-    }
+	private double credValue;
+	private double debtValue;
 
 
-    public double obterSaldo(double credValue, double debtValue){
-        return credValue - debtValue;
-    }
+	public Fornecedor() {}
+	public Fornecedor(String name, String address, String phoneNumber, double credValue, double debtValue){
+		super(name, address, phoneNumber);
+		this.credValue = credValue;
+		this.debtValue = debtValue;
+	}
+	public Fornecedor(String name, double credValue, double debtValue){
+		super(name);
+		this.credValue = credValue;
+		this.debtValue = debtValue;
+	}
 
-    public double getCredValue() {
-        return credValue;
-    }
 
-    public double getDebtValue() {
-        return debtValue;
-    }
+	public double obterSaldo(){
+		return credValue - debtValue;
+	}
 
-    public void setDebtValue(double debtValue) {
-        this.debtValue = debtValue;
-    }
+	public double getCredValue() {
+		return credValue;
+	}
 
-    public void setCredValue(double credValue) {
-        this.credValue = credValue;
-    }
+	public double getDebtValue() {
+		return debtValue;
+	}
+
+	public void setDebtValue(double debtValue) {
+		this.debtValue = debtValue;
+	}
+
+	public void setCredValue(double credValue) {
+		this.credValue = credValue;
+	}
 }
